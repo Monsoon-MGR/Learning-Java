979dfa90420d1e8aeefd9adf8a12c0949a1ca56f
Monsoon-MGR <111853639+Monsoon-MGR@users.noreply.github.com>
2022-09-23 12:05:50 +0300

    Create Main.java

---
+++ literals/Main.java
@@ -0,0 +1,31 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main
+{
+  public static void main (String[]args)
+  {
+    int a = 0b1101010110;
+    int b = 012314;
+    int c = 456;
+    int d = 0x141D12;
+    boolean f = true;
+    boolean g = false;
+    double t = 2.67;
+    float y = 34.456f;
+      System.out.println (f);
+      System.out.println (g);
+      System.out.println ("Text");
+      System.out.println ("double: " + t);
+      System.out.println ("float: " + y);
+      System.out.println ("The number a in binary: " + a);
+      System.out.println ("The number b in the octal system: " + b);
+      System.out.println ("The number c in the decimal system: " + c);
+      System.out.println ("The number d in hexadecimal: " + d);
+  }
+}
